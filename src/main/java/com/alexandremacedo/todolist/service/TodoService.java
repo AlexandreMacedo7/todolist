1a1dde481b4591077d2a7c59b282eb509b90e35f
Alexandre Macedo <alexandre.cst.macedo@gmail.com>
2024-01-20 10:46:58 -0400

    Cria método para retorno de lista de tarefas

---
+++ src/main/java/com/alexandremacedo/todolist/service/TodoService.java
@@ -0,0 +1,31 @@
+package com.alexandremacedo.todolist.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.alexandremacedo.todolist.entity.Todo;
+import com.alexandremacedo.todolist.repository.TodoRepository;
+
+@Service
+public class TodoService {
+    
+
+    private final TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> list(){
+        return todoRepository.findAll(createSortingCriteria());
+    }
+
+    private Sort createSortingCriteria(){
+        Sort sort = Sort.by("priority").descending()
+        .and(Sort.by("name").ascending());
+
+        return sort;
+    }
+}
